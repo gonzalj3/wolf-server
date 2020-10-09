75c6f190446c99abd0316814f9f2d61d79049dd2
gonzalj3 <martin.jose.gonzalez@gmail.com>
2020-10-09 12:46:42 -0500

    trying to get res headers now in the app that goes into socket io

--- src/sockets/indexSockets.js
+++ src/sockets/indexSockets.js
@@ -30,7 +30,7 @@ const setUpSockets = () => {
   };
   //socketApp.use(cors(corsOptions))
   socketApp.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://testwolffe.herokuapp.com");
+    res.header("Access-Control-Allow-Origin", req.headers.origin);
     res.header('Access-Control-Allow-Credentials', true)
     next()
   })
