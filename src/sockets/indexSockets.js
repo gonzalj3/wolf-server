6fd0ebc1ac9b3290ed6c74ab279662d6d3fb5df6
gonzalj3 <martin.jose.gonzalez@gmail.com>
2020-10-10 15:55:54 -0500

    changed it from a port different from  the app to the one the app uses

--- src/sockets/indexSockets.js
+++ src/sockets/indexSockets.js
@@ -14,7 +14,7 @@ const setUpSockets = (app) => {
   const serverWebSocket = http.createServer(app); //use to just pass express// 
   //http.listen(process.env.PORT)
   //https.listen(process.env.WEBSOCKETPORT)
-  serverWebSocket.listen(process.env.WEBSOCKETPORT, () =>
+  serverWebSocket.listen(process.env.PORT, () =>
     console.log(" websocket listening on port " + process.env.WEBSOCKETPORT)
   );
   const io = socketio(serverWebSocket, {transports: ['websocket']}) 
