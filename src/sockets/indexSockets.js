83161eb16d932247693b9539c2999f27de6e7dc5
gonzalj3 <martin.jose.gonzalez@gmail.com>
2020-11-30 08:04:52 -0600

    Changed point system

--- src/sockets/indexSockets.js
+++ src/sockets/indexSockets.js
@@ -468,39 +468,68 @@ const setUpSockets = (app) => {
         console.log(
           "************************** we have updated the scores ***********************"
         );
-
+        let teamPoints = {}
         for (let student of game.roster) {
+          //Find the team that matches the student's team
+          console.log("the student ", student)
+          let team = game.teams.filter((team) => {
+            console.log("team name, student name", team.name, student.team);
+            if (student.team) {
+              return team.name == student.team;
+            } else {
+              return false;
+            }
+          })
+          //Use the team name to find team/key in teamPoints object and then assign to that team's player property either 1 or 1 Plus existing number
+          console.log(`this is the team target ${team[0]}`)
+          if(!teamPoints[team[0].name]){
+            teamPoints[team[0].name] = {name:team[0].name}
+          }
+          let players = teamPoints[team[0].name]["players"] + 1 || 1
+          teamPoints[team[0].name] = 
+          {
+            ...teamPoints[team[0].name],
+            players: players,
+
+          }
+          console.log(`updated teamPoints at ${team[0].name} to ${teamPoints[team[0].name].players} and players is ${players}`)
           //Ensure the student answered the question
           if (student.responses[data.index]) {
             //Ensure the answer is correct
             if (data.answer == student.responses[data.index]) {
-              //Find the team that matches the student's team
-              let team = game.teams.filter((team) => {
-                console.log("team name, student name", team.name, student.team);
-                if (student.team) {
-                  return team.name == student.team;
-                } else {
-                  return false;
-                }
-              });
+
               //Ensure that we have a team to assign points to.
-              if (team[0]) {
-                console.log(
-                  "the team and team score is : ",
-                  team[0],
-                  team[0].score
-                );
-                team[0].score += 1;
-                game.markModified("teams");
-                console.log(
-                  "the team and team score is : ",
-                  team[0],
-                  team[0].score
-                );
+              if (team[0]) {                  
+                teamPoints[team[0].name] = 
+                  {
+                    ...teamPoints[team[0].name],
+                    correct: teamPoints[team[0].name]["correct"] + 1 || 1
+                  }
+                  console.log("have updated teamPoints : ", team[0], teamPoints[team[0].name])
               }
             }
           }
         }
+        for(const team in teamPoints){
+          console.log(`teh team is : ${team}, and the object is ${teamPoints[team].correct}   ${teamPoints[team].players}`)
+          let target = game.teams.filter((teamData) => {
+            console.log(team, teamData.name)
+            return(team === teamData.name)
+          })
+          //console.log("target ", target[0])
+          let scoreUpdate = Math.round(teamPoints[team].correct / teamPoints[team].players * 10) + target[0].score
+          console.log("the score is : ", scoreUpdate)
+
+          if(scoreUpdate){
+            target[0]["score"] = scoreUpdate
+            console.log("target ", JSON.stringify(target))
+            game.markModified("teams");
+            await game.save();
+            console.log(" game ", game.teams)
+          }
+        }
+        //console.log(`the team players ${teamPoints}, and the number correct ${teamPoints.correct}`)
+
         game.lastAction = "point"
 
         await game.save();
